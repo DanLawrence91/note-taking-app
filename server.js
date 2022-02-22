effd474ce1ff25036068a37f5234e64b71c2f50c
Daniel Lawrence <dan.lawrence0810@gmail.com>
2022-02-22 10:36:39 +1030

    console.log middelware added to show path and type of requests

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
 const express = require('express');
 const path = require('path');
+const { clog } = require('./middleware/clog');
 const api = require('./routes/index');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+// middelware used to log to console when different api request made - showing path and type
+app.use(clog);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', api);
 
+// GET route to direct to notes page when button clicked
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+// wildcard route to direct users to landing page
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
